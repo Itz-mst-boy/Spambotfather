546866cf63a840612808ddb69241aad639327cbd
Mukesh <100356452+Itz-mst-boy@users.noreply.github.com>
2022-04-15 20:24:37 +0530

    Update echo.py

--- MukeshxSpam/plugins/echo.py
+++ MukeshxSpam/plugins/echo.py
@@ -10,7 +10,7 @@ from MukeshxSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Ri
 
 from MukeshxSpam import CMD_HNDLR as hl
 from MukeshxSpam.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
-from resources.data import MukeshX
+from resources.data import Mukeshx
 
 
 @Riz.on(events.NewMessage(incoming=True, pattern=r"\%saddecho(?: |$)(.*)" % hl))
